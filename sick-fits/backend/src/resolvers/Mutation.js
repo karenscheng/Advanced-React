55ee84c5e4d1c656197a3f686a12c3047bd86661
Karen-S-Cheng <karen.s.cheng@espn.com>
2019-03-06 08:50:39 -0500

    update is working 🎉 end of 19

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,22 @@ const Mutations = {
         }, info);
 
         return item;    // or you can return the item instead of putting it in a var
+    },
+
+    updateItem(parent, args, ctx, info) {
+        // first take a copy of the updates
+        const updates = {...args};
+        // remove id from the updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id,
+            },
+        }, 
+        info
+        )
     }
 
 };
