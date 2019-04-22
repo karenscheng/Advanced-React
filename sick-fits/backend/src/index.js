790b63c7d3e2226ce4b01d192f86f184122ac383
Karen-S-Cheng <karen.s.cheng@espn.com>
2019-04-22 08:38:04 -0400

    error free. end of 33 I think?

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -10,23 +10,36 @@ const server = createServer();
 server.express.use(cookieParser());
 
 // decode the JWT so we can get the user Id on each request
-server.express.use((req, res, next) => {
-    const {token} = req.cookies;
-    if(token) {
-        const {userId} = jwt.verify(token, process.env.APP_SECRET);
-        // put userId onto req for further requests to access
-        req.userId = userId;
-    }
-    next();
-})
+    server.express.use((req, res, next) => {
+        const {token} = req.cookies;
+        if(token) {
+            const {userId} = jwt.verify(token, process.env.APP_SECRET);
+            // put userId onto req for further requests to access
+            req.userId = userId;
+        }
+        next();
+    });
 
-server.start(
-    {
-        cors: {
-            credentials: true,
-            origin: process.env.FRONTEND_URL,
-        },
-    }, deets => {
-        console.log(`Server is now running on port http://localhost:${deets.port}`);
-    }
-)
+    // create a middleware that populates the user on each request
+
+    server.express.use(async (req, res, next) => {
+        // if they aren't logged in, skip this
+        if (!req.userId) return next();
+        const user = await db.query.user(
+            { where: { id: req.userId } }, 
+            '{ id, permissions, email, name }'
+        );
+        req.user = user;
+        next();
+    });
+
+    server.start(
+        {
+            cors: {
+                credentials: true,
+                origin: process.env.FRONTEND_URL,
+            },
+        }, deets => {
+            console.log(`Server is now running on port http://localhost:${deets.port}`);
+        }
+    );
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,5 +1,5 @@
 const { forwardTo } = require('prisma-binding');
-
+const { hasPermission } = require('../utils');
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
@@ -12,6 +12,16 @@ const Query = {
         return ctx.db.query.user({
             where: { id: ctx.request.userId }
         }, info);
+    },
+    async users(parent, args, ctx, info) {
+        // check if they're logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        // check if the user has the permissions to query all the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        // query all the users
+        return ctx.db.query.users({}, info);
     }
 };
 
